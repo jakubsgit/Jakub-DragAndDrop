ded719d1a950c5221fe99e041d16c6ef0d09d5d5
Jakub Antczak <jakubantczak.info@gmail.com>
2020-03-31 18:49:43 +0200

    working project with splitted modules

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,6 @@
-/// <reference path="models/drag-drop.tsx" />
-/// <reference path="models/project.tsx" />
-/// <reference path="state/project-status.tsx" />
-/// <reference path="util/validation.tsx" />
-/// <reference path="decorators/autobind.tsx" />
-/// <reference path="components/project-input.tsx" />
-/// <reference path="components/project-item.tsx" />
-/// <reference path="components/project-list.tsx" />
+import { ProjectInput } from "./components/project-input.js";
+import { ProjectList } from "./components/project-list.js";
 
-namespace App {
-  new ProjectInput();
-  new ProjectList("active");
-  new ProjectList("finished");
-}
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
